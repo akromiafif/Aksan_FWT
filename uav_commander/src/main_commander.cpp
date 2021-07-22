b7d7b2af69c419635816c26734431465702c0631
akromiafif <massachusetts48@gmail.com>
2021-07-23 04:55:32 +0700

    renew

---
+++ uav_commander/src/main_commander.cpp
@@ -0,0 +1,20 @@
+#include "uav_commander.cpp"
+
+
+
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "uav_commander_node");
+	ros::NodeHandle uav_commander_node;
+	ros::Rate rate(20.0);
+
+	uav_commander::UAVCommander uavCommander(uav_commander_node);
+	uavCommander.setAutoMissionMode();
+	uavCommander.setArm();
+	
+	ROS_INFO("========== READING WAYPOINT ==========");
+	uavCommander.infoWayReached();
+
+	ros::spin();
+
+	return 0;
+}
